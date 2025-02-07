1257f18ed20d6c5fa3a92bb3efeb4e8475734ebe
Mark Valdez <1289198+valdezm@users.noreply.github.com>
2025-02-06 17:02:58 -0800

    Scaffold UI

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -17,16 +17,7 @@ const mockImages = mockImageNames.map((image) => {
 export default function HomePage() {
   return (
     <main className="container mx-auto">
-      Mark Valdez Portfolio
-      <div className="flex flex-wrap gap-4">
-        {mockImages.map((image, index) => {
-          return (
-            <div key={index} className="w-48">
-              <img key={index} src={image.src} alt={image.alt} />
-            </div>
-          )
-        })}
-      </div>
+      Mark Valdez Portfolio Page Layer 2
     </main>
   );
 }
